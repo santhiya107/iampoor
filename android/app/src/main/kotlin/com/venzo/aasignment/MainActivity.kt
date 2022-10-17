cd66c8c781cfd49afb495840142e0c7259f6e06d
santhiya107 <santhiyak@venzotechnologies.com>
2022-10-17 12:27:13 +0530

    oct17

---
+++ android/app/src/main/kotlin/com/venzo/aasignment/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.venzo.aasignment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+     MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:Scaffold (
+        backgroundColor: Colors.white70,
+        appBar: AppBar(
+          title: const Center(child: Text('I am poor')),
+          backgroundColor: Colors.blueAccent,
+
+        ),
+        body:const Center(
+          child: Image (
+          image: NetworkImage('https://images.pexels.com/photos/586744/pexels-photo-586744.jpeg?auto=compress&cs=tinysrgb&w=1600'),
+          ),
+        ),
+
+
+      ),
+
+  ),
+  );
+}
+
